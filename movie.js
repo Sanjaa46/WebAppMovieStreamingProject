0cc0894f2db67c81b1575c0600d87bc33242bf23
Sanjaa46 <sanjaas880@gmail.com>
2024-04-27 20:24:27 +0800

    all file

---
+++ movie.js
@@ -0,0 +1,24 @@
+/*class Movie {
+    constructor(movie) {
+        this.name=movie.name;
+        this.since=movie.since;
+    }
+
+
+    render() {
+        return `
+<article class="movie-small-sized">
+                    <div class="details">
+                        <h3 class="Name">${this.name}</h3>
+                        <p class="year-and-duration"><img src="webimage/movieIcon.png" alt="movie-icon" class="movie-icon"> ${this.since} - 90 мин</p>
+                        <div class="info">
+                            <p>Directed by Jamer Cameron</p>
+                        </div>
+                    </div>
+                    <div class="poster">
+                        <img src="webimage/movies/up.jpg" alt="movie-small-sized-poster">
+                    </div>
+                </article>`;
+    }
+
+}*/
